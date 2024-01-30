52232c9fcce6bb52b8b13ee754a3ef93a9e58fcc
Nikola Pribic <nikola.pribic@menu.app>
2024-01-30 14:07:47 +0100

    Initial Commit

---
+++ NFI Test App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFI Test App
+//
+//  Created by Nikola Pribic on 30.1.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
